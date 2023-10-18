8c7bb63bcd7114c0113cbe3f248743d06b7f2791
fortimindkal <bintangmuhammadhaikal@gmail.com>
2023-10-18 20:18:27 +0700

    Add Obstacle and Mixer Control

---
+++ Assets/Scripts/Audio/AudioMixerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerController : MonoBehaviour
+{
+    public AudioMixer audioMixer; // Reference to your Audio Mixer.
+    public string volumeParameterName = "MasterVolume"; // Use the correct parameter name.
+
+    private bool isMuted = false;
+
+    // Toggle mute on button click or any other event.
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+        audioMixer.SetFloat(volumeParameterName, isMuted ? -80f : 0f); // -80f is usually mute (low volume).
+
+        // You can also store the mute state in a PlayerPrefs or some other way to remember it between sessions.
+    }
+}
